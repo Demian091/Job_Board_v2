a7849a45a6c7acee55d9d033c87341302dc22902
Demian091 <123770496+Demian091@users.noreply.github.com>
2026-04-22 16:05:33 +0100

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -105,11 +105,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.sqlite3',
+
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+    }
+
 }
 # For production (PostgreSQL)
 # import dj_database_url
